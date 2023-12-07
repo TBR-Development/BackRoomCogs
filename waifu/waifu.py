b8d125e7cddb858d016d46e97f330072b0ac0206
Nimbi <JoshuaVL1988@protonmail.com>
2023-12-06 17:44:28 -0700

    Update waifu.py

--- waifu/waifu.py
+++ waifu/waifu.py
@@ -1,8 +1,7 @@
-from redbot.core import commands
+from redbot.core import commands, app_commands
 
-import requests
 import discord
-# import aiohttp
+import aiohttp
 
 class Waifu(commands.Cog):
         """
@@ -12,111 +11,66 @@ class Waifu(commands.Cog):
         def __init__(self, bot):
                 self.bot = bot
 
-                @commands.command()
-                async def waifu(self, ctx):
-                        """
-                        Description: Get a random SFW image
-                        Usage: [p]waifu
-                        """
-                        # retrieve image url from API URL
-                        # ... need to figure out how to convert from requests to aiohttp
 
-                        params = {
-                                'is_nasfw': 'false',
-                                'gif': 'false'
-                        }
+                @app_commands.command()
+                async def waifu(self, interaction: discord.Interaction):
+                        url = 'https://api.waifu.im/search'
+                        params = {'is_nsfw': 'false', 'gif': 'false'}
 
-                        response = requests.get('https://api.waifu.im/search', params=params)
+                        async with aiohttp.ClientSession() as cs:
+                                async with cs.get(url, params=params) as response:
+                                        if response.status == 200:
+                                                data = await response.json()
 
-                        if response.status_code == 200:
-                                data = response.json()
-                        else:
-                                print('Request failed with status code:', response.status_code)
-                        
-                        await ctx.send(data['url'])
+                                                embed = discord.Embed(title = f"{data['name']}", description = f"{data['description']}")
+                                                embed.set_image(url = f"{data['url']}")
+                                                
+                                                await ctx.send(embed=embed)
 
-                @commands.command()
-                @commands.is_nsfw()
-                async def lewds(self, ctx):
-                        """
-                        Description: Get a random NSFW image
-                        Usage: [p]lewds
-                        """
-                        # retrieve image url from API URL
-                        # ... need to figure out how to convert from requests to aiohttp
+                @app_commands.command()
+                async def aniwaifu(self, interaction: discord.Interaction):
+                        url = 'https://api.waifu.im/search'
+                        params = {'is_nsfw': 'false', 'gif': 'true'}
 
-                        params = {
-                                'is_nsfw': 'true',
-                                'gif': 'false'
-                        }
+                        async with aiohttp.ClientSession() as cs:
+                                async with cs.get(url, params=params) as response:
+                                        if response.status == 200:
+                                                data = await response.json()
 
-                        response = requests.get('https://api.waifu.im/search', params=params)
+                                                embed = discord.Embed(title = f"{data['name']}", description = f"{data['description']}")
+                                                embed.set_image(url = f"{data['url']}")
+                                                
+                                                await ctx.send(embed=embed)
 
-                        if response.status_code == 200:
-                                data = response.json()
-                        else:
-                                print('Request failed with status code:', response.status_code)
-                        
-                        await ctx.send(data['url'])
-                
-                @commands.command()
-                async def aniwaifu(self, ctx):
-                        """
-                        Description: Get a random SFW gif
-                        Usage: [p]aniwaifu
-                        """
-                        # retrieve image url from API URL
-                        # ... need to figure out how to convert from requests to aiohttp
 
-                        params = {
-                                'is_nasf': 'false',
-                                'gif': 'true'
-                        }
+                @app_commands.command()
+                @app_commands.is_nsfw()
+                async def lewds(self, interaction: discord.Interaction):
+                        url = 'https://api.waifu.im/search'
+                        params = {'is_nsfw': 'true', 'gif': 'false'}
 
-                        response = requests.get('https://api.waifu.im/search', params=params)
+                        async with aiohttp.ClientSession() as cs:
+                                async with cs.get(url, params=params) as response:
+                                        if response.status == 200:
+                                                data = await response.json()
 
-                        if response.status_code == 200:
-                                data = response.json()
-                        else:
-                                print('Request failed with status code:', response.status_code)
-                        
-                        await ctx.send(data['url'])
-                
-                @commands.command()
-                @commands.is_nsfw()
-                async def anilewds(self, ctx):
-                        """
-                        Description: Get a random NSFW gif
-                        Usage: [p]anilewds
-                        """
-                        # retrieve image url from API URL
-                        # ... need to figure out how to convert from requests to aiohttp
+                                                embed = discord.Embed(title = f"{data['name']}", description = f"{data['description']}")
+                                                embed.set_image(url = f"{data['url']}")
+                                                
+                                                await interaction.response.send_message(embed=embed)
 
-                        params = {
-                                'is_nsfw': 'true',
-                                'gif': 'true'
-                        }
+                @app_commands.command()
+                @app_commands.is_nsfw()
+                async def anilewds(self, interaction: discord.Interaction):
+                        url = 'https://api.waifu.im/search'
+                        params = {'is_nsfw': 'true', 'gif': 'true'}
 
-                        response = requests.get('https://api.waifu.im/search', params=params)
+                        async with aiohttp.ClientSession() as cs:
+                                async with cs.get(url, params=params) as response:
+                                        if response.status == 200:
+                                                data = await response.json()
 
-                        if response.status_code == 200:
-                                data = response.json()
-                        else:
-                                print('Request failed with status code:', response.status_code)
-                        
-                        await ctx.send(data['url'])
-                
-                @commands.Cog.listener()
-                async def on_command_error(self, ctx, error):
-                        if isinstance(error, commands.errors.NSFWChannelRequired):
-                                embed = discord.Embed(
-                                        title = "NSFW Command",
-                                        description = "I am unable to post NSFW content in this channel.\nPlease move to an NSFW channel and try again."
-                                        )
-                                await ctx.send(embed=embed)
-                        else:
-                                embed = discord.Embed(
-                                        title = "Command Error",
-                                        description = error.args[0]
-                                        )
-                                await ctx.send(embed=embed)
+                                                embed = discord.Embed(title = f"{data['name']}", description = f"{data['description']}")
+                                                embed.set_image(url = f"{data['url']}")
+                                                
+                                                await interaction.response.send_message(embed=embed)
