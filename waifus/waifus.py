b63ece6a054a1c7bb0351d38c7888e4162ed52ce
Joshua Lewis <JoshuaVL1988@protonmail.com>
2024-01-08 21:48:57 -0700

    Update waifus.py

--- waifus/waifus.py
+++ waifus/waifus.py
@@ -43,7 +43,7 @@ class Waifus(commands.Cog):
                 embed = discord.Embed(
                         title="Cog Information",
                         description=box(
-                                f"{'Cog Author':<11}: {author}\n{'Cog Version':<10}: {version}",
+                                f"{'Cog Author':<11}: {author}\n{'Cog Version':<11}: {version}\n{'Cog Repo':<11}: {self.__repo__}",
                                 lang="yaml",
                         ),
                         color=await ctx.embed_color(),
