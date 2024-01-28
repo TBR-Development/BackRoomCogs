703b933f51f15140310eb9b14f22b81ab010c681
Joshua Lewis <JoshuaVL1988@protonmail.com>
2024-01-28 01:02:43 -0700

    Update purge cog permissions

--- purge/purge.py
+++ purge/purge.py
@@ -18,6 +18,7 @@ class Purge(commands.Cog):
         
         @commands.command()
         @commands.bot_has_permissions(manage_messages=True, send_messages=True)
+        @commands.has_permissions(manage_messages=True)
         async def purge(self, ctx, num_messages: int):
                 """Purge <n> amount of messages from the current channel"""
 
