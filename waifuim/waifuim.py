f19047adaed73f5d5e3504e595c7f058ecef946e
Joshua Lewis <JoshuaVL1988@protonmail.com>
2024-02-13 20:28:39 -0700

    Fix a typo

--- waifuim/waifuim.py
+++ waifuim/waifuim.py
@@ -29,7 +29,7 @@ class Waifu(commands.Cog):
         @commands.bot_has_permissions(send_messages=True, embed_links=True)
         async def waifuim(self, ctx):
           if ctx.invoked_subcommand is None:
-            help_cmd = self.bot.get_command('hwlp')
+            help_cmd = self.bot.get_command('help')
             await ctx.invoke(help_cmd, command='waifuim')
         
         @waifuim.command()
