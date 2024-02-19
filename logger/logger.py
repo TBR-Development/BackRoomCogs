eac8a3b5ce4c090e27009252a90e766bf44e1cfa
Nimbi <JoshuaVL1988@outlook.com>
2024-02-18 23:35:55 -0700

    ...

--- logger/logger.py
+++ logger/logger.py
@@ -41,7 +41,7 @@ class Logger(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         """
-        Log guild join events to the logger channel if it is set
+        Log guild add events
         """
         
         date = datetime.now()
@@ -50,10 +50,28 @@ class Logger(commands.Cog):
         
         logger_channel = self.bot.get_channel(await self.config.logger_channel())
             
-        e = discord.Embed(title='Logger', description='The bot has been added to a new guild.', timestamp=date, color=discord.Color.blue())
+        e = discord.Embed(title='Logger', description='The bot has been added to a guild.', timestamp=date, color=discord.Color.blue())
         e.add_field(name='Guild Name', value=guild.name, inline=True),
         e.add_field(name='Guild ID', value='||{}||'.format(guild.id), inline=True)
-        e.add_field(name='Join Date', value=join_date, inline=False)
+        e.add_field(name='Date Added', value=join_date, inline=False)
+        e.set_footer(text=self.bot.user.name, icon_url=self.bot.user.display_avatar.url)
+        await logger_channel.send(embed=e)
+        
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        """
+        Log guild remove events
+        """
+        
+        date = datetime.now()
+        
+        leave_date = date.strftime('%a %d %b %Y, %I:%M %p')
+        
+        logger_channel = self.bot.get_channel(await self.config.logger_channel())
+            
+        e = discord.Embed(title='Logger', description='The bot has been removed from a guild.', timestamp=date, color=discord.Color.blue())
+        e.add_field(name='Guild Name', value=guild.name, inline=True),
+        e.add_field(name='Date Removed', value=leave_date, inline=False)
         e.set_footer(text=self.bot.user.name, icon_url=self.bot.user.display_avatar.url)
         await logger_channel.send(embed=e)
         
