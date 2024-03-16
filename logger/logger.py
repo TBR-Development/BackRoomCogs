cfff5d4df2c267214503390fe62c796aad15297a
Joshua Lewis <JoshuaVL1988@protonmail.com>
2024-03-16 11:20:41 -0700

    ...
    Signed-off-by: Joshua Lewis <JoshuaVL1988@protonmail.com>

--- logger/logger.py
+++ logger/logger.py
@@ -63,8 +63,8 @@ class Logger(commands.Cog):
             
         embed = discord.Embed(title='Logger Settings')
         embed.color = await ctx.embed_color()
-        embed.add_field(name='Enabled', value=logger_enabled)
-        embed.add_field(name='Channel', value=box(logs_channel))
+        embed.add_field(name='Enabled', value=logger_enabled, inline=True)
+        embed.add_field(name='Channel', value=logs_channel, inline=True)
         embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.display_avatar.url)
         
         await ctx.send(embed=embed)
